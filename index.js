b395d52eb065a5340bd63243ce011173d0b734c9
Hal Henke <halhenke@gmail.com>
2015-09-18 03:30:31 +1000

    Loader converted to ES6

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var jade = require("react-jade");
-var React = require("react");
-var loaderUtils = require("loader-utils");
+const jade = require('react-jade');
+// const React = require('react');
+const loaderUtils = require('loader-utils');
 
-module.exports = function(source) {
+// module.exports = function(source) {
+export default function (source) {
   this.cacheable && this.cacheable();
   // options
-  var loaderOptions = loaderUtils.parseQuery(this.query);
-  
-  var template = jade.compile(source, loaderOptions);
-  var requireString = "var React = require('react');";
-  return requireString + "\n" + "module.exports = " + template.toString();
-};
+  const loaderOptions = loaderUtils.parseQuery(this.query);
+
+  const template = jade.compile(source, loaderOptions);
+  const requireString = 'var React = require("react");';
+  return `${requireString}\nmodule.exports = ${template.toString()}`;
+  // var requireString = 'var React = require("react");';
+  // return requireString + '\n' + 'module.exports = ' + template.toString();
+}
