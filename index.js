586d2b409c2ab1ef28512790541e95d9c9b2ff00
Hal Henke <halhenke@gmail.com>
2015-09-21 22:38:18 +1000

    Clean up main file

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const jade = require('react-jade');
-// const React = require('react');
 const loaderUtils = require('loader-utils');
 
-// module.exports = function(source) {
 export default function (source) {
   this.cacheable && this.cacheable();
-  // options
   const loaderOptions = loaderUtils.parseQuery(this.query);
 
   const template = jade.compile(source, loaderOptions);
   const requireString = 'var React = require("react");';
   return `${requireString}\nmodule.exports = ${template.toString()}`;
-  // var requireString = 'var React = require("react");';
-  // return requireString + '\n' + 'module.exports = ' + template.toString();
 }
